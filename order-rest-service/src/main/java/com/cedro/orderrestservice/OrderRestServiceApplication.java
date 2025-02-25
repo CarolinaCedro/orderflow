011afcdd2bf53ccda311dc449f887ee3a3762032
cedromaxxsoft <ana.carolina@maxxsoft.com.br>
2025-02-25 19:45:45 -0300

    create a new ms of abstraction rest

---
+++ order-rest-service/src/main/java/com/cedro/orderrestservice/OrderRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cedro.orderrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderRestServiceApplication.class, args);
+	}
+
+}
---
+++ order-rest-service/src/test/java/com/cedro/orderrestservice/OrderRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cedro.orderrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
