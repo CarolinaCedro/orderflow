1d54ccccc0a0a6c9e9c7667e884e5230a5d4263e
CarolinaCedro <yotechgirl@gmail.com>
2025-09-09 18:44:38 -0300

    alinhando

--- order-service/src/main/java/org/cedro/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/org/cedro/orderservice/OrderServiceApplication.java
@@ -5,12 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EntityScan(basePackages = {"org.cedro.ordermodel"})
-@EnableFeignClients(basePackages = {
+@ComponentScan(basePackages = {
         "org.cedro.orderutils.feign.viacep"
+})
+@EnableFeignClients(basePackages = {
+
 })
 public class OrderServiceApplication {
 
--- order-service/src/main/java/org/cedro/orderservice/controler/OrdemController.java
+++ order-service/src/main/java/org/cedro/orderservice/controler/OrdemController.java
@@ -1,7 +1,13 @@
 package org.cedro.orderservice.controler;
 
+import com.cedro.orderrestservice.rest.controller.AbstractControllerJpa;
+import com.cedro.orderrestservice.rest.service.impl.AbstractServiceJpa;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.cedro.ordermodel.domains.models.Order;
 import org.cedro.orderservice.events.OrderCreatedEvent;
 import org.cedro.orderservice.service.OrderService;
+import org.cedro.orderservice.service.impl.OrderServiceImpl;
 import org.cedro.orderutils.feign.viacep.record.Endereco;
 import org.cedro.orderutils.feign.viacep.service.ViaCep;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/orderflow/v1/order")
-public class OrdemController {
+public class OrdemController  {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
