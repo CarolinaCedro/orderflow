0967097cfa4c89809967302a411e55e8ed3e3d63
CarolinaCedro <yotechgirl@gmail.com>
2026-01-16 17:34:22 -0300

    ++

--- order-service/src/main/java/org/cedro/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/org/cedro/orderservice/OrderServiceApplication.java
@@ -13,9 +13,9 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {
         "org.cedro.orderutils.feign.viacep"
 })
-@EnableFeignClients(basePackages = {
-
-})
+//@EnableFeignClients(basePackages = {
+//
+//})
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
